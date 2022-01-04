0116b239d3282ce69c317f75e29b627aad0de015
AnawaratneMANA <it19086408@my.sliit.lk>
2022-01-04 11:10:59 -0500

    Header component created.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
 import Head from 'next/head';
+import Header from '../components/Header';
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Hulu</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1> Empty NEXTJS Application.</h1>
+
+      {/* Header */}
+      <Header/>
+
+      {/* Navigation Bar */}
+      
+
+      {/* Card Layout */}
     </div>
   );
 }

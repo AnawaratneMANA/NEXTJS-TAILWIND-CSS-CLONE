85a48c3217d4d63d1adbff05215095c79224655a
AnawaratneMANA <it19086408@my.sliit.lk>
2022-01-04 11:17:14 -0500

    Image domain configuration added.

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+function Header() {
+    return (
+        <header className="">
+            <Image src="http://links.papareact.com/ua6" width={200} height={100}/>
+        </header>
+    )
+}
+
+export default Header

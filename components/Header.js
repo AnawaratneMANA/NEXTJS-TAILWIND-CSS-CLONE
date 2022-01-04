47272bf07506cc2cefa0dfafd271e43d7a830687
AnawaratneMANA <it19086408@my.sliit.lk>
2022-01-04 12:07:47 -0500

    Header Icons Imported. Updated.

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,26 @@
 import Image from "next/image";
 import huluImage from '../Images/hulu-image.png'
 import HeaderItem from "./HeaderItem";
-import {HomeIcon} from "@heroicons/react/outline";
+import {
+    HomeIcon,
+    LightningBoltIcon,
+    BadgeCheckIcon,
+    CollectionIcon,
+    SearchIcon,
+    UserIcon
+
+} from "@heroicons/react/outline";
 
 function Header() {
     return (
       <div>
         <div>
           <HeaderItem title="HOME" Icon={HomeIcon} />
+          <HeaderItem title="TRANDING" Icon={LightningBoltIcon} />
+          <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+          <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+          <HeaderItem title="SEARCH" Icon={SearchIcon} />
+          <HeaderItem title="ACCOUNT" Icon={UserIcon} />
         </div>
         <header className="">
           <Image

46d64f001ddacad29a6b296a0d7d6aa95b82e3fe
AnawaratneMANA <it19086408@my.sliit.lk>
2022-01-04 11:52:59 -0500

    Icon dependecies added.

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,24 @@
 import Image from "next/image";
 import huluImage from '../Images/hulu-image.png'
+import HeaderItem from "./HeaderItem";
+import {HomeIcon} from "@heroicons/react/outline";
 
 function Header() {
     return (
+      <div>
+        <div>
+          <HeaderItem title="HOME" Icon={HomeIcon} />
+        </div>
         <header className="">
-            <Image className="object-contain" 
-            src={huluImage} 
-            width={200} 
-            height={100}/>
+          <Image
+            className="object-contain"
+            src={huluImage}
+            width={200}
+            height={100}
+          />
         </header>
-    )
+      </div>
+    );
 }
 
 export default Header
---
+++ components/HeaderItem.js
@@ -0,0 +1,6 @@
+function HeaderItem({Icon, title}) {
+    return (
+        <div> This is header item.</div>
+    )
+}
+export default HeaderItem

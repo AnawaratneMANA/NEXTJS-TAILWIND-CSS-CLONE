05681147b11638259bf33709d7aa8aeba8bcdad4
AnawaratneMANA <it19086408@my.sliit.lk>
2022-01-05 12:57:41 -0500

    Item count increased.

--- utils/request.js
+++ utils/request.js
@@ -3,5 +3,17 @@ export default {
     fetchTrending: {
         title: 'Trending',
         url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchSomething: {
+        title: 'Action',
+        url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchMore: {
+        title: 'Comedy',
+        url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchMore2: {
+        title: 'Horror Movies',
+        url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
     }
 }

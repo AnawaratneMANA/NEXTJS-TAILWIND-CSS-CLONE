c932795aef18f708184a405afaccf4c5ae48cc3c
AnawaratneMANA <it19086408@my.sliit.lk>
2022-01-05 12:55:38 -0500

    Styles added to the Navigation.

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,12 +1,21 @@
-import request from "../utils/request"
+import request from "../utils/request";
 
 function Navigation() {
   return (
     <nav>
-      <div>
-          {Object.entries(request).map(([key, {title, url}]) => (
-              <h2>{title}</h2>
-          ))}
+      <div className="flex px-10 sm: px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20">
+        {Object.entries(request).map(([key, { title, url }]) => (
+          <h2
+            key={key}
+            className="cursor-pointer 
+              transition duration-100 transform 
+              hover:scale-125
+            hover:text-white 
+            active:text-red-500"
+          >
+            {title}
+          </h2>
+        ))}
       </div>
     </nav>
   );

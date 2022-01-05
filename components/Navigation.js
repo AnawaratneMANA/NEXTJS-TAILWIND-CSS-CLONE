1986c63b6cacdab8df541deac5d41a203b2e7c39
AnawaratneMANA <it19086408@my.sliit.lk>
2022-01-05 12:43:14 -0500

    Keywords Imported from a request file.

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,10 +1,15 @@
+import request from "../utils/request"
+
 function Navigation() {
-    return (
-        <nav>
-          <div></div>  
-        </nav>
-    )
+  return (
+    <nav>
+      <div>
+          {Object.entries(request).map(([key, {title, url}]) => (
+              <h2>{title}</h2>
+          ))}
+      </div>
+    </nav>
+  );
 }
 
-export default Navigation
-
+export default Navigation;

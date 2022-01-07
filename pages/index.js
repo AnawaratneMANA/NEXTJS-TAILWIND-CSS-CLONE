35a1e9c32caad6eab7bd06d2d53b646504a62713
AnawaratneMANA <it19086408@my.sliit.lk>
2022-01-07 11:31:37 -0500

    API data fetch API data not working.

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,9 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import Navigation from '../components/Navigation';
 import Results from '../components/Results';
-export default function Home() {
+import requests from "../utils/request"
+export default function Home(props) {
+  console.log(props)
   return (
     <div>
       <Head>
@@ -20,4 +22,18 @@ export default function Home() {
       <Results/>
     </div>
   );
+}
+
+// Server Side rendering function
+export async function getServerSideProps(context){
+  const genre = context.query.genre;
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+    requests[genre]?.url || requests.fetchTrending.url}`).then((res) => res.json());
+
+    return {
+      props: {
+        results: request.results,
+      },
+    };
 }
--- utils/request.js
+++ utils/request.js
@@ -4,28 +4,4 @@ export default {
         title: 'Trending',
         url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
     },
-    fetchSomething: {
-        title: 'Action',
-        url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
-    },
-    fetchMore: {
-        title: 'Comedy',
-        url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
-    },
-    fetchMore3: {
-        title: 'Horror Movies',
-        url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
-    },
-    fetchMore6: {
-        title: 'Horror Movies',
-        url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
-    },
-    fetchMore8: {
-        title: 'Horror Movies',
-        url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
-    },
-    fetchMore9: {
-        title: 'Horror Movies',
-        url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
-    },
 }
